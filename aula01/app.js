c63957ac129110d191b47eec8cc500aa378b3bf9
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-07-27 10:15:21 -0300

    uso de acento grave (crase)

--- aula01/app.js
+++ aula01/app.js
@@ -1,6 +1,10 @@
 const nome = 'Arthur'
 const sobrenome = 'Piloto'
 
-const nomeCompleto = nome + ' ' + sobrenome
+// const nomeCompleto = nome + ' ' + sobrenome
 
-console.log(nomeCompleto)
+// Essa String reconhece variáveis e funções
+// Os dois comandos, liha 4 e linha 8, fazem a mesma coisa. Entretanto, o formato da linha 8 é melhor.
+const nomeCompleto = `${nome} ${sobrenome}`
+
+console.log(nomeCompleto2)
