73ba62a38366b1e8b12f345494b3330a1a512993
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-07-27 11:05:31 -0300

    interação com o usuário fora do console

--- aula01/app.js
+++ aula01/app.js
@@ -6,13 +6,14 @@
 function juntarNome (){
     const nome = document.getElementById('nome').value
     const sobrenome = document.getElementById('sobrenome').value
+    const resultado = document.getElementById('resultado')
     
     // Essa String reconhece variáveis e funções
     // Os dois comandos fazem a mesma coisa. Entretanto, o segundo formato é melhor.
     // const nomeCompleto = nome + ' ' + sobrenome
-    const nomeCompleto = `${nome} ${sobrenome}`
+    const nomeCompleto = `Seu nome é: ${nome} ${sobrenome}`
     
-    console.log('Seu nome é ' + nomeCompleto)
+    resultado.textContent = nomeCompleto
 }
 
 document.getElementById('juntar').addEventListener('click', juntarNome)
