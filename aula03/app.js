0f5034010543b998ffc3fda8c0ea527581a73025
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-31 09:41:44 -0300

    adding more parameters

--- aula03/app.js
+++ aula03/app.js
@@ -4,6 +4,6 @@ import { showArray } from "./showArray.js"
 
 const numbers = [23, 5, 9, 10, 100, 45]
 
+showArray(numbers, `main`, `ALL NUMBERS`)
 showArray(numbers, `main`)
-showArray(numbers, `main`)
-showArray(numbers, `main`)
+showArray(numbers, `main`, `TEST`)
--- aula03/showArray.js
+++ aula03/showArray.js
@@ -1,13 +1,13 @@
 `use strict`
 
-const showArray = (numbers, element) => {
+const showArray = (numbers, element, title = `ARRAY`) => {
     const container = document.querySelector(element)
     const div = document.createElement(`div`)
     div.classList.add(`numbers-container`)
 
     // Preenchendo a DIV com os elementos do Array
     div.innerHTML = `
-        <h2>Test</h2>
+        <h2>${title}</h2>
         <div class="numbers-item">
             ${numbers.join(`</div><div class="numbers-item">`)}
         </div>
