575b01622f9eced109efedd1b9226a318a08f1f1
Arthur Piloto <72941675+arthurpiloto@users.noreply.github.com>
2022-08-31 08:24:13 -0300

    working with import and export

--- aula03/app.js
+++ aula03/app.js
@@ -1,6 +1,7 @@
 `use strict`
 
+import { showArray } from "./showArray.js"
+
 const numbers = [23, 5, 9, 10, 100, 45]
 
-const showArray = () => alert(`Olá Mundo`) 
 showArray(numbers)
---
+++ aula03/showArray.js
@@ -0,0 +1,8 @@
+`use strict`
+
+const showArray = () => alert(`Olá Mundo`)
+
+// Criação de um objeto para importar as funções de um arquivo
+export {
+    showArray
+}
